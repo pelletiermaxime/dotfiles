5f891f81f637d932eaae8b59c278983d9fbab913
Maxime Pelletier <pelletiermaxime@gmail.com>
2013-12-13 22:09:27 -0500

    Move my custom aliases out of .zshrc to clean it up

---
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,13 @@
+alias glp="git log -p"
+alias gg="git grep"
+
+alias koha='$HOME/koha koha'
+alias kohasudo='$HOME/koha mpelletier'
+alias record='ffmpeg -f x11grab -s 1920x1080 -r 3 -b 50k -i :0.0+3360 -vcodec libx264 /home/max/tmp/screencapture/`date +%F`.mkv'
+alias sl='sl'
+alias watson='watson -s dirty'
+
+alias -g grp='| grep -si'
+alias -s log="tail -f"
+
+alias screen='screen -U'
